d89bee2b9602e97c2d602c7dce8588ed7036af8d
Crystal Craig <ckcraig@amarillocollege.com>
2025-04-01 13:56:06 -0500

    update

--- practice.py
+++ practice.py
@@ -1,1 +1,14 @@
 print('hello')
+
+hour=14
+greeting1 = "Good morning"
+greeting2 = "Good afternoon"
+greeting3 = 'Good evening'
+if hour<12:
+    print(greeting1)
+elif hour<18:
+    print(greeting2)
+else: 
+    print(greeting3)
+       
+
